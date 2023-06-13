6b59b8685fe0e1406d49278f105a78f595da435d
Madiwka3 <madi.turgunov.03@gmail.com>
2023-06-13 14:35:21 +0600

    Added... a thing

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -16,6 +16,25 @@ public class WeatherController : Controller
         keys = JObject.Parse(System.IO.File.ReadAllText("App_Data/keys.json"));
         
     }
+    public IActionResult GetFlowerWeather()
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get,
+            "https://api.openweathermap.org/data/2.5/weather?lat=16.40&lon=120.59&appid=" + keys["weather"]);
+        var client = _clientFactory.CreateClient();
+        var response = client.Send(request);
+        if (response.IsSuccessStatusCode)
+        {
+            var responseString = response.Content.ReadAsStringAsync();
+            JObject json = JObject.Parse(responseString.Result);
+            return Json(new { success = true, temp = ((int)json["main"]["temp"] - 273) });
+        }
+        else
+        {
+            _logger.LogError("Error: {StatusCode}", response.StatusCode);
+            return Json(new { success = false, temp = 0 });
+        }
+        
+    }
     // GET
     public async Task<IActionResult> Index()
     {
